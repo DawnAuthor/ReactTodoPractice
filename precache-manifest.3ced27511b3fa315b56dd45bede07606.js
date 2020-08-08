b340e1ff2e8b6b92abb82c104e3479d12aa6b3eb
Darryl Taylor <contact.dtaylor@gmail.com>
2020-08-08 14:45:20 -0400

    Updates

---
+++ precache-manifest.3ced27511b3fa315b56dd45bede07606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9873f7657f1cbcf889ea6f1458ef3271",
+    "url": "/ReactTodoPractice/index.html"
+  },
+  {
+    "revision": "791dd34691ed0818b421",
+    "url": "/ReactTodoPractice/static/css/main.0ccff844.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/ReactTodoPractice/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodoPractice/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "791dd34691ed0818b421",
+    "url": "/ReactTodoPractice/static/js/main.8cf1796e.chunk.js"
+  },
+  {
+    "revision": "e1ba648eefa0d35f33b4",
+    "url": "/ReactTodoPractice/static/js/runtime-main.fd155cbd.js"
+  }
+]);
